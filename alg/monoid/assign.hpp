59000cee8d401f3cdc7fb9006b6b316a19054d94
Channing <1012889296@qq.com>
2023-03-06 16:28:03 +0800

    Revert "(use "git restore --staged <file>..." to unstage)"
    This reverts commit 1f5b593623f87198f49d3f0c6c604e8c22937dcc.

--- alg/monoid/assign.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-template <typename X, X none_val>
-struct Monoid_Assign {
-    using value_type = X;
-    static X op(X x, X y) { return (y == none_val ? x : y); }
-    static constexpr X unit() { return none_val; }
-    static constexpr bool commute = false;
-};
